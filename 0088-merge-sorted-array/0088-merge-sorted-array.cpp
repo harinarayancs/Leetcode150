47d726e244433b311871f1c3547f47e233175212
harinarayancs <164733376+harinarayancs@users.noreply.github.com>
2024-06-22 16:43:41 +0530

    Time: 2 ms (49.53%), Space: 11 MB (71.93%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,31 +1,24 @@
 class Solution {
 public:
    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-  // i points to the last element of nums1 (excluding the 0s)
-  int i = m - 1;
-  // j points to the last element of nums2
-  int j = n - 1;
-  // k points to the last element in the merged array (nums1)
-  int k = m + n - 1;
+   int i = m - 1; // Last element in the actual part of nums1
+    int j = n - 1; // Last element in nums2
+    int k = m + n - 1; // Last position in nums1
 
-  // Compare elements from nums1 and nums2 and insert the larger one into nums1 at the end (pointed to by k)
-  while (i >= 0 && j >= 0) {
-    if (nums1[i] >= nums2[j]) {
-      nums1[k] = nums1[i];
-      i--;
-    } else {
-      nums1[k] = nums2[j];
-      j--;
+    // While there are elements to be considered in both arrays
+    while (i >= 0 && j >= 0) {
+        if (nums1[i] > nums2[j]) {
+            nums1[k--] = nums1[i--];
+        } else {
+            nums1[k--] = nums2[j--];
+        }
     }
-    k--;
-  }
 
-  // Copy the remaining elements of nums2 (if any) to nums1
-  while (j >= 0) {
-    nums1[k] = nums2[j];
-    j--;
-    k--;
-  }
+    // If there are still elements in nums2
+    while (j >= 0) {
+        nums1[k--] = nums2[j--];
+    }
+
+    // No need to consider remaining elements in nums1 because they are already in place
 }
-
 };
