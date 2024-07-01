c7cac85a5f913e4e49a3ab9747788945fb3cc3ae
harinarayancs <164733376+harinarayancs@users.noreply.github.com>
2024-07-01 12:48:10 +0530

    Time: 12 ms (90.17%), Space: 21.2 MB (16.04%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -3,37 +3,48 @@ class MinStack {
 private:
     std::stack<int> stack;
     std::stack<int> minstack;
-
 public:
-    MinStack() {}
-
+    MinStack() {
+        
+    }
+    
     void push(int val) {
         stack.push(val);
-        if (minstack.empty() || val <= minstack.top()) {
+        if(minstack.empty() || val<=minstack.top()){
             minstack.push(val);
         }
     }
-
+    
     void pop() {
-        if (!stack.empty()) {
-            if (stack.top() == minstack.top()) {
-                minstack.pop();
-            }
-            stack.pop();
+        if(!stack.empty()&& !minstack.empty()){
+        if(stack.top()==minstack.top()){
+            minstack.pop();
+        }
+        stack.pop();
         }
     }
-
+    
     int top() {
-        if (!stack.empty()) {
+        if(!stack.empty()){
             return stack.top();
-        }
-        return -1;  // Handle error case when stack is empty
+         }
+        return -1;
+        
     }
-
+    
     int getMin() {
-        if (!minstack.empty()) {
+         if(!minstack.empty()){
             return minstack.top();
-        }
-        return -1;  // Handle error case when minstack is empty
+         }
+        return -1;
     }
 };
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
